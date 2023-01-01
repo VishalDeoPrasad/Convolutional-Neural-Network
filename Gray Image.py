8ccd59b1e4a43bb3d659a020451a67a09aafbf5c
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-01 18:37:13 +0530

    update

--- Gray Image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread("opencv-logo.png")
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("image", img)
-cv2.imshow("Gray Image", gray_img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()

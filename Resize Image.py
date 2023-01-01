dbe764e191b23c710be9a2e2aa8ae5762b78eb6e
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-01 18:57:23 +0530

    Resize the image

--- Resize Image.py
+++ Resize Image.py
@@ -1,11 +1,23 @@
 import cv2
 
-img = cv2.imread("opencv-logo.png")
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-resize_img = cv2.resize(img, (0,0), fx=0.6, fy=0.6)
+org_img = cv2.imread("opencv-logo.png")
+#resize_img = cv2.resize(img, (0,0), fx=0.6, fy=0.6)
 
-cv2.imshow("image", resize_img)
-cv2.imshow("Gray Image", gray_img)
+# downscale the image using new width and height
+down_width = 300
+down_height = 200
+down_points = (down_width, down_height)
+resized_down = cv2.resize(org_img, down_points, interpolation= cv2.INTER_LINEAR)
+ 
+# upscale the image using new width and height
+up_width = 600
+up_height = 400
+up_points = (up_width, up_height)
+resized_up = cv2.resize(org_img, up_points, interpolation= cv2.INTER_LINEAR)
+
+cv2.imshow("Original Image", org_img)
+cv2.imshow("Downscale image", resized_down)
+cv2.imshow("Upscale Image", resized_up)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

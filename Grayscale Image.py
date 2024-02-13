cd3a5d0dea7e9666096ece88d17f99ed96f549ef
Vishal Kumar <vishaldeoprasad@yahoo.com>
2024-02-13 13:03:43 +0530

    Update Grayscale Image.py

--- Grayscale Image.py
+++ Grayscale Image.py
@@ -5,5 +5,6 @@ gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("image", img)
 cv2.imshow("Gray Image", gray_img)
 
+# this is to hold the window
 cv2.waitKey(0)
 cv2.destroyAllWindows()
